7927f9cb9c393991aa2a3618d28c5b2c6f0c1061
miraj <miraj00@gmail.com>
2022-09-11 01:36:38 -0400

    components, services and interface created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ExpenseFormComponent } from './components/expense-form/expense-form.component';
+import { ExpenseListComponent } from './components/expense-list/expense-list.component';
+import { TotalExpensesComponent } from './components/total-expenses/total-expenses.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExpenseFormComponent,
+    ExpenseListComponent,
+    TotalExpensesComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/total-expenses/total-expenses.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-total-expenses',
+  templateUrl: './total-expenses.component.html',
+  styleUrls: ['./total-expenses.component.css']
+})
+export class TotalExpensesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
